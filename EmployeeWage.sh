3d314dd0edb0fe5a01e5b1345ba21c69eae33168
Rachana R <rachanarrachu28@gmail.com>
2020-09-09 20:29:47 +0530

    Added:employee wage calculation is added

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,10 +2,16 @@
 echo "Welcome to Employee Wage Computation"
 
 isPresent=1;
+empWagePerHr=20
+empFullTimeHr=8
 randomCheck=$(( RANDOM%2 ))
+salary=0
 if [ $isPresent -eq $randomCheck ]
 then
         echo "Employee is Present"
+	salary=$(( $empWagePerHr * $empFullTimeHr ))
+
 else
         echo "Employee is Absent"
 fi
+echo $salary
