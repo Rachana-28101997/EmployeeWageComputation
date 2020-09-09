16c7111445400a16b770e9c6e609ccec010d519b
Rachana R <rachanarrachu28@gmail.com>
2020-09-09 20:52:56 +0530

    Added:Part time employee and his wage is calculated

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,17 +1,31 @@
-#! /bin/bash -x
-echo "Welcome to Employee Wage Computation"
+#! /bin/bash
+echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
 
 isPresent=1;
 empWagePerHr=20
 empFullTimeHr=8
 randomCheck=$(( RANDOM%2 ))
 salary=0
+isFullTime=1
+isPartTime=2
+empPartTimeHrs=4
 if [ $isPresent -eq $randomCheck ]
 then
         echo "Employee is Present"
-	salary=$(( $empWagePerHr * $empFullTimeHr ))
+        randCheck=$((RANDOM%3))
 
+        if [ $isFullTime -eq $randCheck ]
+        then
+                salary=$(( $empFullTimeHr * $empWagePerHr ))
+                echo "The salary of Full Time Employee is:" $salary
+        elif [ $isPartTime -eq $randCheck ]
+        then
+                salary=$(( $empPartTimeHrs * $empWagePerHr ))
+                echo "The salary of Part Time Employee is:" $salary
+        fi
 else
         echo "Employee is Absent"
+        salary=0
+        echo "employee salary is :" $salary
+
 fi
-echo $salary
