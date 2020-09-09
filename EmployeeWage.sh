acfb4c0fcd799a7257bb1a735756566598683df4
Rachana R <rachanarrachu28@gmail.com>
2020-09-09 21:13:00 +0530

    Added:Employee wage calculated using switch case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,29 +3,24 @@ echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
 
 isPresent=1;
 empWagePerHr=20
-empFullTimeHr=8
 randomCheck=$(( RANDOM%2 ))
 salary=0
 isFullTime=1
 isPartTime=2
-empPartTimeHrs=4
 if [ $isPresent -eq $randomCheck ]
 then
-        echo "Employee is Present"
-        randCheck=$((RANDOM%3))
-
-        if [ $isFullTime -eq $randCheck ]
-        then
-                salary=$(( $empFullTimeHr * $empWagePerHr ))
-                echo "The salary of Full Time Employee is:" $salary
-        elif [ $isPartTime -eq $randCheck ]
-        then
-                salary=$(( $empPartTimeHrs * $empWagePerHr ))
-                echo "The salary of Part Time Employee is:" $salary
-        fi
-else
-        echo "Employee is Absent"
-        salary=0
-        echo "employee salary is :" $salary
-
+randCheck=$((RANDOM%3))
+case $randCheck in
+        $isFullTime)
+                empHr=8
+        ;;
+        $isPartTime)
+                empHr=4
+        ;;
+        *)
+                empHr=0
+        ;;
+esac
 fi
+salary=$(($empHr * $empWagePerHr))
+echo $salary
